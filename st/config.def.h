f052cf2e05bafe59cc9f3f9829d69d5d8690a1c5
h3ll <h3ll@sideros.org>
2025-04-09 17:04:21 +0200

    AGJIODSJGOIPSJPGD JGHAHAHAHAH HAHAHHAH HAHHAHA HAHAHHAHAH AHHHAHAHAH arch linux

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=25:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
@@ -116,12 +116,12 @@ static const char *colorname[] = {
 	"white",
 
 	[255] = 0,
-    
+
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-    "#cdd6f4", // fg
-    "#11111e" // bg
+	"gray90", /* default foreground colour */
+	"black", /* default background colour */
 };
 
 
@@ -201,9 +201,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-    { ShiftMask,            XK_Page_Up,      kscrollup,      {.i = 20} },
-    { ShiftMask,            XK_Page_Down,    kscrolldown,    {.i = 20} },
-
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
